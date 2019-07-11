625a8d8bf137b2add24813851d295212eaff8d15
Marcus Jean-Louis <marcusjnls@gmail.com>
2019-07-11 10:09:00 +0300

    create basic construct of code

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+}; //this adds two numbers
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = add(number1, number2);
+alert(result);
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+}; //this subtacts two numbers
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = subtract(number1, number2);
+alert(result);
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};//this multiplies two numbers
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = multiply(number1, number2);
+alert(result);
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};//this divides two numbers
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = divide(number1, number2);
+alert(result);
